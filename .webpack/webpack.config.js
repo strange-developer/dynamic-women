249220a13b6a52a9a58e1146c5c0ea9727419432
Unknown <cbodasing@gmail.com>
2019-04-15 21:53:23 +0200

    Auto stash before merge of "feature/setup" and "origin/feature/setup"

--- .webpack/webpack.config.js
+++ .webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const join = path.join.bind(null, __dirname, '..');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
